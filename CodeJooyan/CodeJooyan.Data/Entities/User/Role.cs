4f16d58da05c3722a4da03c70f9efca36bc14259
Dorin <dyaeco13841384@outlook.com>
2023-03-02 21:52:01 +0330

    user tbls created role user userrole

---
+++ CodeJooyan/CodeJooyan.Data/Entities/User/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CodeJooyan.Data.Entities.User
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+        [Display(Name = "")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(ErrorMessage ="{0} نمیتواند بیشتر از {1} باشید")]
+        public string RoleTitle { get; set; }
+    }
+}
---
+++ CodeJooyan/CodeJooyan.Data/Entities/User/UserRole.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CodeJooyan.Data.Entities.User
+{
+    public class UserRole
+    {
+        #region Ctor
+        public UserRole()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        
+        [Key]
+        public int UR_Id { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+        
+        #endregion
+
+        #region Relations
+
+
+
+
+        #endregion
+    }
+}
